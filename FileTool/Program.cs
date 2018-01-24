03e1a9d20e99e4071b69e1489783af436f936e1b
Oleg Filippov <35500737+folegv75@users.noreply.github.com>
2018-01-24 20:49:12 +0300

    s

--- FileTool/Program.cs
+++ FileTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,12 +15,57 @@ namespace FileTool
         {
             if (!File.Exists(pFilename))
             {
-                pErrorMessage = $"Файл не найден: {pFilename}";
-                return false;
+                string saveErrorMessage;
+                    var Res = SaveSettings(pFilename, out saveErrorMessage);
+                if (Res)
+                {
+                    pErrorMessage = $"Файл не найден: {pFilename}. Файл создан.";
+                    return false;
+                }
+                else
+                {
+                    pErrorMessage = $"Файл не найден: {pFilename} Ошибка создания {saveErrorMessage}";
+                    return false;
+                }
+
+            }
+
+            using (StreamReader stream = new StreamReader(pFilename))
+            {
+                try
+                {
+                    string Data = stream.ReadToEnd();
+                    CommandParameter = JsonConvert.DeserializeObject<WorkParameter>(Data);
+                }
+                catch (Exception ex)
+                {
+                    pErrorMessage = "Ошибка чтения параметров:" + ex.ToString();
+                    return false;
+                }
             }
             pErrorMessage = null;
             return true;
 
+        }
+
+        public static bool SaveSettings(string pFilename, out string pErrorMessage)
+        {
+
+            using (StreamWriter stream = new StreamWriter(pFilename))
+            {
+                try
+                {
+                    string Data = JsonConvert.SerializeObject(CommandParameter, Formatting.Indented);
+                    stream.Write(Data);
+                }
+                catch (Exception ex)
+                {
+                    pErrorMessage = "Error write settings:" + ex.ToString();
+                    return false;
+                }
+            }
+            pErrorMessage = null;
+            return true;
         }
 
     }
